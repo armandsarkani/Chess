249fc1c4f3890f760ae5f6265e7b18a733fdaa28
Marvis Nguyen <marvisn@uci.edu>
2019-01-23 12:54:20 -0800

    Added Board struct, modified some structs

--- src/Board.h
+++ src/Board.h
@@ -10,6 +10,7 @@
 
 typedef struct Player PLAYER;
 typedef struct Piece PIECE;
+typedef struct Boards BOARDS;
 
 typedef enum PieceName {Pawn1, Pawn2, Pawn3, Pawn4, Pawn5, Pawn6, Pawn7, Pawn8, Rook1, Rook2, Bishop1, Bishop2, Knight1, Knight2, Queen, King} pieces;
 
@@ -27,13 +28,23 @@ struct Piece{
     int value; // AI
 };
 
-void InitializeBoard(PLAYER *p1, PLAYER *p2);
+struct Boards{
+	PLAYER *player;
+	PIECE *pieceboard[8][8];
+};
+
+void InitializeBoard(PLAYER *p1, PLAYER *p2, BOARDS *boards);
 void DrawBoard(void);
 void MakeMove(PLAYER *p);
 PIECE *CreatePiece(int r, int c, char piece, char color, PLAYER *player);
 void MovePiece(PIECE *piece, int newr, int newc);
 int AlphatoNum(char alpha);
+char NumtoAlpha(int num);
 PIECE *CheckPiece(PLAYER *p, int r, int c);
 int FindEmptySpace(int r, int c);
+BOARDS *CreateBoards(void);
+PIECE *FindPiece(BOARDS *boards, int r, int c);
+void PieceInfo(PIECE *piece);
+
 
 #endif /* Board_h */
