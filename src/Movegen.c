b62ff35ecb4207ede138228b0143bbf45a300dc8
Benny Lin <bennyl6@uci.edu>
2019-01-25 02:44:26 -0800

    Modules for functions that generate list of moves for player

---
+++ src/Movegen.c
@@ -0,0 +1,84 @@
+
+/*Last modified 1/24/19*/
+/*Things that need to be added:
+	DeleteMoveList
+	DeleteMoveEntry
+	
+	accompanying header file movegen.h
+*/
+
+#include "Pieces.h"
+#include "Board.h"
+
+void getmoves(char *org_board[8][8],PLAYER *player, PLAYER *oppenent, MOVELIST *list){
+	char *cpy_board;
+	char piece;
+	int success; /*if success = 0*/
+	
+	for loop{/* copies board into a temp board*/
+		for loop{
+			cpy_board[i][j] = org_board[i][j]; //i = r, j = c
+		/*end for*/
+	/*end for*/
+		}
+	}
+	for loop{/* scans board of pieces, once it reaches a piece, checks if piece is AI controlled, then makes move on temp board 
+		i: dummy variable for coordinate tracking on chess board*/
+		for loop{ /*j: dummy variable for coordinate tracking on chess board*/
+			if(board!= NULL){	
+				piece = CheckPiece(player, i, j);
+				if(piece != NULL){
+
+					for loop {/*x: dummy variable for coordinate tracking on chess board*/
+						for loop{/*y: dummy variable for coordinate tracking on chess board*/
+						success = CallPiece(player, piece, i, j, x, y);
+						if (success != 1){ /*if AI_piece does not return a failure*/
+							MovePiece(cpy_board[8][8], piece, x, y);/*makes move on cpyboard*/
+							AddLegalMoves(list, i, j, x, y, cpy_board[8][8]);
+							for loop{/*a: dummy variable for coordinate tracking on chess board*/
+								for loop{ /*b: : dummy variable for coordinate tracking on chess board*/
+								cpy_board[a][b] = org_board[8][8]; /*resets copyboard for next move*/ 
+								}
+							}
+						}
+					}
+				}
+				}
+			
+			}
+				
+			}
+		}
+	}
+					}*/
+		/*end for*/
+	/*end for*/
+}
+
+
+
+void AddLegalMoves(MOVELIST *list, src_row, src_col, dest_row, dest_col, board[8][8]){
+/*Adds move information into the given list, allocating space and making new entries; stores resulting board from making the move*/	
+	MOVE *new_entry = malloc();
+	assert(new_entry);
+	list->last->nextentry = new_entry;
+	new_entry->preventry = list->last;
+	list->last= new_entry;
+	
+	new_entry->src_row = src_row;
+	new_entry->src_col = src_col;
+	new_entry->dst_row = dest_row;
+	new_entry->dst_col = dest_col;
+	
+	for loop{
+		for loop{
+			new_entry -> newboard[i][j] = board[i][j];
+		}
+	}
+}
+
+
+
+
+
+
