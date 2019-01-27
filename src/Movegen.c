19ecb6eafef4b9162c3c0ea4a00103cc77f80929
Benny Lin <bennyl6@uci.edu>
2019-01-27 11:58:38 -0800

    Added Newlist, DeleteList, DelteMove. Changed AI.c to fit

--- src/Movegen.c
+++ src/Movegen.c
@@ -14,7 +14,7 @@ void getmoves(char *org_board[8][8],PLAYER *player, PLAYER *oppenent, MOVELIST *
 	char *cpy_board;
 	char piece;
 	int success; /*if success = 0*/
-	
+		
 	for loop{/* copies board into a temp board*/
 		for loop{
 			cpy_board[i][j] = org_board[i][j]; //i = r, j = c
@@ -59,26 +59,82 @@ void getmoves(char *org_board[8][8],PLAYER *player, PLAYER *oppenent, MOVELIST *
 
 void AddLegalMoves(MOVELIST *list, src_row, src_col, dest_row, dest_col, board[8][8]){
 /*Adds move information into the given list, allocating space and making new entries; stores resulting board from making the move*/	
-	MOVE *new_entry = malloc();
+	
+	MOVE *new_entry = malloc(sizeof(MOVE));
 	assert(new_entry);
-	list->last->nextentry = new_entry;
-	new_entry->preventry = list->last;
-	list->last= new_entry;
+
+	if( list->first = NULL){
+		list->first = new_entry;
+		new_entry->preventry = NULL;
+		new_entry->nextentry = NULL;
+		
+		new_entry->src_row = src_row;
+		new_entry->src_col = src_col;
+		new_entry->dst_row = dest_row;
+		new_entry->dst_col = dest_col;
+		
+		list->last = new_entry;
 	
-	new_entry->src_row = src_row;
-	new_entry->src_col = src_col;
-	new_entry->dst_row = dest_row;
-	new_entry->dst_col = dest_col;
-	
-	for loop{
 		for loop{
-			new_entry -> newboard[i][j] = board[i][j];
+			for loop{
+				new_entry -> newboard[i][j] = board[i][j];
+			}
+		}
+		
+	
+	}else{
+		list->last->nextentry = new_entry;
+		new_entry->preventry = list->last;
+		list->last= new_entry;
+
+		new_entry->src_row = src_row;
+		new_entry->src_col = src_col;
+		new_entry->dst_row = dest_row;
+		new_entry->dst_col = dest_col;
+	
+		for loop{
+			for loop{
+				new_entry -> newboard[i][j] = board[i][j];
+			}
 		}
 	}
 }
 
+MOVELIST *NewMoveList(void){
+	MOVELIST *l;
+	l = malloc(sizeof(MOVELIST));
+	if (!l)
+	{
+		perror("\n Out of Memory! Aborting...");
+		exit(10);
+	}
+	l->first = NULL;
+	l->last = NULL;
+	return l;
+}
 
-
+////Deletes list of all possible legal moves: frees memory
+void DeleteMoveList(MOVELIST *list){
+	assert(list);
+	MOVE *next;
+	MOVE *temp;
+	temp = list->first;
+	while(temp) {
+		next = temp->nextentry;
+		DeleteMoveEntry(temp);
+		temp = next;
+	}
+	free(list);
+}
+////Deletes a single move entry within the movelist
+void DeleteMoveEntry(MOVE *entry){
+	assert(entry);
+	if (entry->next_level != NULL){
+		DeleteMoveList(next_level);
+	}else {
+		free(entry);
+	}
+}
 
 
 
--- src/Movegen.h
+++ src/Movegen.h
@@ -10,14 +10,45 @@
 	
 */
 
+
+typedef struct move MOVE;
+typedef struct movelist MOVELIST;
+
+struct move {
+	int src_row;
+	int src_col;
+	int dst_row;
+	int dst_col;
+	MOVE *nextentry;
+	MOVE *preventry;
+	char newboard[8][8];
+	int score;
+	MOVELIST *next_level;
+};
+struct movelist {
+	MOVE *first;
+	MOVE *last;
+}
 void getmoves(char *org_board[8][8],PLAYER *player, PLAYER *oppenent, MOVELIST *list);
 //Adds entries for possible legal moves into a given list by evaluating a given board config and player perspective
 //
 void AddLegalMoves(MOVELIST *list, src_row, src_col, dest_row, dest_col, board[8][8]){
 ////Adds move information into the given list, allocating space and making new entries; stores resulting board from making the move
-//
-void DeleteMoveList()
+
+
+
+}
+
 ////Deletes list of all possible legal moves: frees memory
-//
-void DeleteMoveEntry()
+void DeleteMoveList(MOVELIST *list){
+
+}
+
 ////Deletes a single move entry within the movelist
+void DeleteMoveEntry(MOVE *entry){
+
+}
+
+
+
+
